12b065a06fc14083d02094c051030e0dcd0ac2c0
don <donovandall@icloud.com>
2021-02-18 10:44:15 +0000

    test

--- src/discord.rs
+++ src/discord.rs
@@ -14,7 +14,8 @@ pub const DISCORD_REGISTER_URL: &str = "https://discordapp.com/api/v6/auth/regis
 pub const DISCORD_LIST_GUILDS: &str = "https://discordapp.com/api/v6/users/@me/guilds";
 pub const TOPEST_DISCORD_INVITE_LINK: &str = "47PDSBM2";
 pub const HABIBI_DISCORD_INVITE_LINK: &str = "QQBb2JcUdF";
-pub const DISCORD_INVITE_LINK: &str = TOPEST_DISCORD_INVITE_LINK;
+pub const MEMES_DISCORD_INVITE_LINK: &str = "TFAq8FZ";
+pub const DISCORD_INVITE_LINK: &str = MEMES_DISCORD_INVITE_LINK;
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
